d688c5ab0a0a71bec65019b6f888e7a25f79fdd5
Евгений Сергеев <akaevgeniy@yandex.ru>
2022-04-09 12:28:52 +0300

    fix: dotenv config

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,6 +1,7 @@
 const { NODE_ENV, JWT_SECRET } = process.env;
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const User = require('../models/user');
 const NotFoundError = require('../errors/not-found-err');
 const BadRequestError = require('../errors/bad-req-err');
